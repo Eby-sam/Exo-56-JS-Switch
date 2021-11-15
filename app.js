88ea349dfa09e4e1470539e34a89c4cded05216a
coque <coquelet.samuel@gmail.com>
2021-11-15 10:34:51 +0100

    fini

---
+++ app.js
@@ -0,0 +1,28 @@
+let matos = prompt(' quelles matérielle utilisez-vous ?');
+
+switch (matos) {
+
+    case 'souris':
+        alert('j\'utilise une'+ ' ' + matos)
+        break;
+
+    case 'clavier':
+        alert('j\'utilise un'+ ' ' + matos);
+        break;
+
+    case 'ordinateur':
+        alert('j\'utilise un'+ ' ' + matos)
+        break;
+
+    case 'tapis':
+        alert('j\'utilise un'+ ' ' + matos)
+        break;
+
+    case 'imprimante':
+        alert('j\'utilise une'+ ' ' + matos);
+        break;
+
+    case 'ondulateur':
+        alert('j\'utilise un'+ ' ' + matos);
+        break;
+}
